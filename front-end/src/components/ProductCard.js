fe9a063f5155fd564e2a768305fa39e912532763
sam-gupta-git <sam78748@gmail.com>
2025-10-29 07:42:20 -0500

    more various styling changes

--- front-end/src/components/ProductCard.js
+++ front-end/src/components/ProductCard.js
@@ -7,6 +7,7 @@ export default function ProductCard({ product, onAdd }) {
       <div className="product-image" style={{ backgroundImage: `url(${product.image})` }} />
       <div className="product-body">
         <h3 className="product-title" title={product.name}>{product.name}</h3>
+        <div className="product-divider"></div>
         <p className="product-price">${product.price.toFixed(2)}</p>
         <button className="btn-add" onClick={() => onAdd(product)}>Add to cart</button>
       </div>
